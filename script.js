ba0caf34077ebc9254e5bbcb59b4e1cf38695d2b
ramiAlrahawi <115189965+ramiAlrahawi@users.noreply.github.com>
2025-02-23 20:45:35 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     // اختيار عنصر الأيقونة (menu-icon) باستخدام الـ class
     const menuIcon = document.querySelector('.menu-icon');
 
@@ -11,14 +11,14 @@ document.addEventListener('DOMContentLoaded', function() {
     // التحقق من وجود العناصر قبل إضافة المستمعات
     if (menuIcon && closeIcon && navHeader) {
         // إضافة مستمع حدث للنقر على menu-icon
-        menuIcon.addEventListener('click', function() {
+        menuIcon.addEventListener('click', function () {
             // تغيير الكلاس إلى nav-active
             navHeader.classList.remove('nav-bar');
             navHeader.classList.add('nav-active');
         });
 
         // إضافة مستمع حدث للنقر على close-icon
-        closeIcon.addEventListener('click', function() {
+        closeIcon.addEventListener('click', function () {
             // إعادة الكلاس إلى nav-bar
             navHeader.classList.remove('nav-active');
             navHeader.classList.add('nav-bar');
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 document.querySelectorAll('.menu-items a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
+    anchor.addEventListener('click', function (e) {
         e.preventDefault(); // منع السلوك الافتراضي للرابط
 
         const targetId = this.getAttribute('href').substring(1); // استخراج ID القسم
